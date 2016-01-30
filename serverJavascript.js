80bbf7c34b92be20be7041470fc65e5914534a34
schae586 <schae586@morris.umn.edu>
2016-01-30 13:31:19 -0600

    added letter converter to serverJavascript.js

--- serverJavascript.js
+++ serverJavascript.js
@@ -9,3 +9,33 @@ exports.numChecker = function(int){
 }
 
 
+exports.gradeLetterConverter = function(str){
+    if (str == "A") {
+        return 4.00 % 1;
+    } else if(str == "A-"){
+        return 3.67 % 1;
+    } else if(str == "B+") {
+        return 3.33 % 1;
+    } else if(str == "B") {
+        return 3.00 % 1;
+    } else if(str == "B-") {
+        return 2.67 % 1;
+    } else if(str == "C+") {
+        return 2.33 % 1;
+    } else if(str == "C") {
+        return 2.00 % 1;
+    } else if(str == "C-") {
+        return 1.67 % 1;
+    } else if(str == "D+") {
+        return 1.33 % 1;
+    } else if(str == "D") {
+        return 1.00 % 1;
+    } else if(str == "D-") {
+        return 0.67 % 1;
+    } else if(str == "F") {
+        return 0.00 % 1;
+    } else {
+        return "Error, invalid grade entry";
+    }
+
+}
